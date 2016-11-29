e766231dad278e980098ccb9d667e567ed1030c8
Devorah Langsam <devorah@langsam.org>
2016-11-29 15:58:08 -0500

    Add comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161118174611) do
+ActiveRecord::Schema.define(version: 20161129205633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "user_name"
+    t.string   "user_email"
+    t.text     "content"
+    t.integer  "movie_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "images", force: :cascade do |t|
     t.string   "url"
