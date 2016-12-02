89f6f603a8e296dbfbf9108c105f93cbc87a8c5e
Devorah Langsam <devorah@langsam.org>
2016-12-02 15:14:47 -0500

    Add comments api to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 	root to: "static#index"
 	get '/about', to: "static#about"
   scope '/api' do
-    resources :movies
+    resources :movies do
+    	resources :comments, only: [:show, :create]
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
